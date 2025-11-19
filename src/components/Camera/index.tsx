d072282693f436e9370f854dca380d5bb60f2557
Peter Calabrese <peter@calabrese.dev>
2025-11-19 13:55:15 -0500

    add image overlay

--- src/components/Camera/index.tsx
+++ src/components/Camera/index.tsx
@@ -1,10 +1,27 @@
+import { useState } from "react";
 import { UseCamera } from "../../hooks/useCamera";
 import styles from "./camera.module.css";
 function Camera() {
   const { error, isActive, startCamera, stopCamera, videoRef } = UseCamera();
+  const [image, setImage] = useState("");
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      // Create a temporary URL for the file
+      const imageUrl = URL.createObjectURL(file);
+      setImage(imageUrl);
 
+      // Optional: If camera is running, stop it when uploading a file to save battery
+      if (isActive) stopCamera();
+    }
+  };
   return (
     <div className={styles.container}>
+      <input
+        type="file"
+        title="Upload Image"
+        onChange={(e) => handleFileChange(e)}
+      />
       <div className={styles.viewfinder}>
         <video
           ref={videoRef}
@@ -12,6 +29,7 @@ function Camera() {
           playsInline // Crucial for iOS
           muted
         />
+        {image && <img src={image} />}
         {error && <div className="error-msg">{error}</div>}
       </div>
 
